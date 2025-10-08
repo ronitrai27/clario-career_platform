049c792f053f84460249281eac8ec797ddf07d9d
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-10-08 18:13:21 +0530

    profile

--- clario-SIH/src/app/(main)/home/profile/page.tsx
+++ clario-SIH/src/app/(main)/home/profile/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import { useUserData } from "@/context/UserDataProvider";
 import { LucideActivity, LucideEdit } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import { LuCopy } from "react-icons/lu";
 
 const UserProfile = () => {
   const { user } = useUserData();
@@ -35,6 +39,172 @@ const UserProfile = () => {
           height={80}
           className="w-[120px] h-[120px] absolute top-10 right-5 rounded-full opacity-20"
         />
+      </div>
+      {/* ALL INFO */}
+      <div className="mt-16 px-12">
+        <h2 className=" text-2xl font-semibold font-inter">
+          Personal Information
+        </h2>
+        <div className="space-y-3 mt-6">
+          <div className="flex items-center max-w-[440px] gap-8">
+            <Label className="font-sora text-base" htmlFor="email">
+              Name
+            </Label>
+            <Input
+              id="name"
+              type="text"
+              value={user?.userName}
+              readOnly
+              className="bg-white w-full font-raleway"
+            />
+          </div>
+          <div className="flex items-center max-w-[440px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="email"
+            >
+              Email
+            </Label>
+            <Input
+              id="email"
+              type="text"
+              value={user?.userEmail}
+              readOnly
+              className="bg-white w-full font-raleway"
+            />
+          </div>
+          <div className="flex items-center max-w-[440px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="email"
+            >
+              Phone No.
+            </Label>
+            <Input
+              id="phone"
+              type="text"
+              value={user?.userPhone}
+              readOnly
+              className="bg-white w-full font-inter"
+            />
+          </div>
+           <div className="flex items-center max-w-[50px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="email"
+            >
+              Invite Link
+            </Label>
+            <Input
+              id="phone"
+              type="text"
+              value={user?.invite_link}
+              readOnly
+              className="bg-white w-full font-inter"
+            />
+            <LuCopy className="cursor-pointer" size={26} />
+          </div>
+        </div>
+        <Separator className="my-6 max-w-[700px]" />
+        <h2 className=" text-2xl font-semibold font-inter">
+          Basic Information
+        </h2>
+        <div className="space-y-3 mt-6">
+          <div className="flex items-center max-w-[600px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="email"
+            >
+              Institute/College Name
+            </Label>
+            <Input
+              id="name"
+              type="text"
+              value={user?.institutionName}
+              readOnly
+              className="bg-white w-full font-raleway"
+            />
+          </div>
+          <div className="flex items-center max-w-[450px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="current_status"
+            >
+              Current Occupation
+            </Label>
+            <Input
+              id="current_status"
+              type="text"
+              value={user?.current_status}
+              readOnly
+              className="bg-white w-full font-raleway"
+            />
+          </div>
+          <div className="flex items-center max-w-[450px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="mainFocus"
+            >
+              Main Focus
+            </Label>
+            <Input
+              id="mainFocus"
+              type="text"
+              value={user?.mainFocus}
+              readOnly
+              className="bg-white w-full font-inter"
+            />
+          </div>
+        </div>
+        <Separator className="my-6 max-w-[700px]" />
+        <h2 className=" text-2xl font-semibold font-inter">Work Information</h2>
+         <div className="space-y-3 mt-6">
+          <div className="flex items-center max-w-[600px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="email"
+            >
+              Choosed Career 
+            </Label>
+            <Input
+              id="name"
+              type="text"
+              value={user?.institutionName}
+              readOnly
+              className="bg-white w-full font-raleway"
+            />
+          </div>
+          <div className="flex items-center max-w-[450px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="current_status"
+            >
+              Current Occupation
+            </Label>
+            <Input
+              id="current_status"
+              type="text"
+              value={user?.current_status}
+              readOnly
+              className="bg-white w-full font-raleway"
+            />
+          </div>
+          <div className="flex items-center max-w-[450px] gap-8">
+            <Label
+              className="font-sora text-base whitespace-nowrap"
+              htmlFor="mainFocus"
+            >
+              Main Focus
+            </Label>
+            <Input
+              id="mainFocus"
+              type="text"
+              value={user?.mainFocus}
+              readOnly
+              className="bg-white w-full font-inter"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
