76d6e621c29991d0c0f1135775fdcf84ff374927
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-10-19 14:10:18 +0530

    Home design

--- clario-SIH/src/app/(main)/_components/ActionBox.tsx
+++ clario-SIH/src/app/(main)/_components/ActionBox.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { GraduationCap, Map, Users, FileText } from "lucide-react";
 import Link from "next/link";
 import { LuChevronDown, LuNewspaper, LuVideo } from "react-icons/lu";
-import { is } from "zod/v4/locales";
+import { motion, AnimatePresence } from "framer-motion";
 
 type Profession =
   | "10th Student"
@@ -19,15 +19,16 @@ export default function ActionBox() {
 
   return (
     <div className="my-12">
-      <h2 className="font-sora text-[24px]  tracking-tight mb-4 pl-7 " onClick={()=>setIsOpen(!isOpen)}>
+      <h2
+        className="font-sora text-[24px]  tracking-tight mb-4 pl-7 "
+        onClick={() => setIsOpen(!isOpen)}
+      >
         Quick Actions{" "}
-        {
-          isOpen ? (
-            <LuChevronDown className="inline-block rotate-180 cursor-pointer text-blue-500" />
-          ) : (
-            <LuChevronDown className="inline-block  cursor-pointer text-blue-500" />
-          )
-        }
+        {isOpen ? (
+          <LuChevronDown className="inline-block rotate-180 cursor-pointer text-blue-500" />
+        ) : (
+          <LuChevronDown className="inline-block  cursor-pointer text-blue-500" />
+        )}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-7 px-6 max-w-[1000px] mx-auto">
         {/* 1*/}
@@ -86,9 +87,17 @@ export default function ActionBox() {
             </div>
           </div>
         </Link>
-        {/* OTHERS ----------------------- */}
+      </div>
+      {/* OTHERS ----------------------- */}
+      <AnimatePresence>
         {isOpen && (
-          <>
+          <motion.div
+            initial={{ opacity: 0, height: 0, y: -20 }}
+            animate={{ opacity: 1, height: "auto", y: 0 }}
+            exit={{ opacity: 0, height: 0, y: -20 }}
+            transition={{ duration: 0.3, ease: "linear" }}
+            className="grid grid-cols-3 gap-7 px-6 max-w-[1000px] mx-auto mt-7"
+          >
             <Link href="/home/interview-prep">
               <div className="relative overflow-hidden rounded-md shadow-lg px-3 py-2 text-black hover:scale-105 transition bg-gradient-to-br from-white via-white to-pink-50 cursor-pointer border-l-8 border-amber-500">
                 <div className="flex  items-center gap-3">
@@ -114,7 +123,7 @@ export default function ActionBox() {
                     <Users className="w-6 h-6 text-green-600" />
                   </div>
                   <h3 className="text-lg font-medium leading-tight font-inter">
-                   Career Board
+                    Career Board
                   </h3>
                 </div>
                 <div>
@@ -128,10 +137,10 @@ export default function ActionBox() {
             {/* 6 */}
             <div className="relative overflow-hidden rounded-md shadow-lg px-3 py-2 text-black hover:scale-105 transition bg-gradient-to-br from-white to-purple-100 cursor-pointer  border-l-8 border-purple-500">
               <div className="flex  items-center gap-4">
-                <div className="p-2 bg-blue-500/15 rounded-full backdrop-blur-sm">
+                <div className="p-2 bg-purple-500/15 rounded-full backdrop-blur-sm">
                   <FileText className="w-6 h-6 text-purple-500" />
                 </div>
-                <h3 className="text-lg font-semibold font-raleway">
+                <h3 className="text-lg font-medium font-inter">
                   AI Resume Maker
                 </h3>
               </div>
@@ -142,9 +151,9 @@ export default function ActionBox() {
                 </p>
               </div>
             </div>
-          </>
+          </motion.div>
         )}
-      </div>
+      </AnimatePresence>
     </div>
   );
 }
--- clario-SIH/src/app/(main)/home/page.tsx
+++ clario-SIH/src/app/(main)/home/page.tsx
@@ -65,6 +65,7 @@ import { BsSuitcase, BsSuitcase2 } from "react-icons/bs";
 import SuggestedCollegeScroll from "../_components/SuggestedCollegeScroll";
 import MessageNamesList from "./test/showchats/page";
 import confetti from "canvas-confetti";
+import AiSuggestedBoxHome from "../_components/AiSuggestedBoxHome";
 
 const fallbackAvatars = [
   "/a1.png",
@@ -300,95 +301,7 @@ export default function HomePage() {
             </div>
 
             {/* AI SUGGESSTIONS  */}
-            {user?.isQuizDone ? (
-              <div className="border border-rose-400 bg-gradient-to-br from-rose-50 via-rose-50 to-rose-300 rounded-md  w-[820px] h-60 mx-auto mt-12 relative overflow-hidden">
-                <div className="flex relative h-full">
-                  {/* Left side */}
-                  <Image
-                    src="/static1.png"
-                    alt="Decorative Element"
-                    width={300}
-                    height={300}
-                    className="object-comtain absolute left-0 -bottom-12 z-50"
-                  />
-
-                  <Image
-                    src="/static5.png"
-                    alt="Decorative Element"
-                    width={300}
-                    height={300}
-                    className=" absolute -left-2 -top-10 "
-                  />
-                  {/* RIGHT SIDE */}
-
-                  <div className="absolute -bottom-16 right-0 w-16 h-16">
-                    <div className="w-full h-full bg-white/40 rounded-tr-2xl rotate-45 transform origin-top-right"></div>
-                  </div>
-
-                  <div className="absolute bottom-20 -right-10 w-16 h-16">
-                    <div className="w-full h-full bg-white/30 rounded-tr-2xl rotate-6 transform origin-top-right"></div>
-                  </div>
-
-                  <div className="absolute -bottom-10 left-[45%] w-16 h-16">
-                    <div className="w-full h-full bg-white/40 rounded-tr-2xl rotate-6 transform origin-top-right"></div>
-                  </div>
-
-                  <div className="w-[64%] ml-auto h-full py-4 px-3">
-                    <h2 className="font-sora text-3xl font-extrabold tracking-tight text-pretty text-center capitalize leading-normal bg-gradient-to-br from-slate-800 via-rose-500 to-rose-300 text-transparent bg-clip-text">
-                      It&apos;s time to take your career to the next level and
-                      shine!
-                    </h2>
-
-                    <div className="flex items-center gap-5 mt-5 justify-center">
-                      <Button className="bg-white text-black font-inter text-sm cursor-pointer hover:bg-gray-100">
-                        Check Job Listings <LuInbox />
-                      </Button>
-                      <Button className="bg-white text-black font-inter text-sm cursor-pointer hover:bg-gray-100">
-                        Check Courses <LuBookOpenCheck />
-                      </Button>
-                    </div>
-
-                    <div className="flex items-center gap-5 mt-5 justify-center">
-                      <Button className="bg-white text-black font-inter text-sm cursor-pointer hover:bg-gray-100">
-                        Go To Career Board <LuWorkflow />
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="border border-dashed border-blue-500 bg-blue-50 rounded-md  pt-4 px-4 w-[820px] mx-auto mt-12">
-                <h2 className="text-[22px] font-semibold font-sora text-center">
-                  Complete Your Quiz To Unlock Your Career Potential
-                </h2>
-                <div className="flex justify-center my-8">
-                  <div className="bg-white w-16 h-16 rounded-full flex items-center justify-center">
-                    <LuCircleFadingPlus className="text-blue-500" size={40} />
-                  </div>
-                </div>
-                <div className="w-full my-6 flex justify-center">
-                  <Button className="bg-gradient-to-br from-blue-400 to-blue-500 hover:from-blue-400 hover:to-blue-600 text-white">
-                    Get Started <LuActivity className="ml-2" />
-                  </Button>
-                </div>
-                <div className="flex items-center justify-between">
-                  <Image
-                    src="/element8.png"
-                    alt="Decorative Element"
-                    width={200}
-                    height={200}
-                    className=" object-cover -mt-48 -ml-3"
-                  />
-                  <Image
-                    src="/element7.png"
-                    alt="Decorative Element"
-                    width={200}
-                    height={200}
-                    className=" object-cover -mt-48"
-                  />
-                </div>
-              </div>
-            )}
+           <AiSuggestedBoxHome/>
           </div>
           {/* --------------Right side--------------- */}
           <div className="w-[24%]  flex flex-col gap-14  items-center h-full ">
